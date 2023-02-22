7cd3df98d1cce01d02f8b5149561fb7d2cc2048e
howardwen <xxhowwiexx@gmail.com>
2023-02-21 20:24:50 -0500

    added new function

--- python_file.py
+++ python_file.py
@@ -8,3 +8,6 @@ class Example:
     # Defining __call__ method
     def __call__(self):
         print("Instance is called via special method")
+
+    def newfunc(self):
+        print("This is a new function")
